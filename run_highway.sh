66b1e4dedc4e3702283310968709311fc5238417
Anurag Koul <koulanurag@gmail.com>
2019-02-25 16:33:56 -0800

    clean-up (obsolute files)

--- run_highway.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python main_highway.py --total_episodes 400 --lr 0.01 --train_interval 25 --eval_episodes 5 --no_cuda --train
-#echo "Testing ..."
-#python main_highway.py --total_episodes 400 --lr 0.0001 --train_interval 25 --eval_episodes 20 --no_cuda --test
-#echo "MSX ..."
-#python main_highway.py --total_episodes 400 --lr 0.0001 --train_interval 25 --eval_episodes 10 --no_cuda --eval_msx
